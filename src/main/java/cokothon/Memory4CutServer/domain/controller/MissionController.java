72d6bd04619e1805ead7757d67fa061f5900890a
jun02160 <jun020216@naver.com>
2023-12-26 23:16:47 +0900

    [FEAT] 미션 인증 업로드 API #7

---
+++ src/main/java/cokothon/Memory4CutServer/domain/controller/MissionController.java
@@ -0,0 +1,37 @@
+package cokothon.Memory4CutServer.domain.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import cokothon.Memory4CutServer.domain.dto.request.AchieveMissionRequest;
+import cokothon.Memory4CutServer.domain.dto.response.GetGroupPhotoResponse;
+import cokothon.Memory4CutServer.domain.service.MissionService;
+import cokothon.Memory4CutServer.global.common.response.ApiResponse;
+import cokothon.Memory4CutServer.global.common.response.SuccessType;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/group/{groupId}/mission")
+@RequiredArgsConstructor
+public class MissionController {
+
+	private final MissionService missionService;
+
+	@PatchMapping("/upload")
+	@ResponseStatus(HttpStatus.OK)
+	public ApiResponse<GetGroupPhotoResponse> uploadMissionAchieve(
+			@RequestPart MultipartFile image,
+			@RequestBody AchieveMissionRequest request,
+			@PathVariable Long groupId) {
+
+		return ApiResponse.success(SuccessType.UPLOAD_MISSION_ACHIEVE_SUCCESS, missionService.achieveMission(request, image, groupId));
+	}
+}
