3bd56b4ae76bb007ac244184b639aaa6cc69b99d
jun02160 <jun020216@naver.com>
2023-12-26 16:22:48 +0900

    :tada: Project Init Settings

---
+++ src/main/java/cokothon/Memory4CutServer/Memory4CutServerApplication.java
@@ -0,0 +1,13 @@
+package cokothon.Memory4CutServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Memory4CutServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Memory4CutServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cokothon/Memory4CutServer/Memory4CutServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cokothon.Memory4CutServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Memory4CutServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
